88bfa4a0abe4e9180241f47869f82610b23424e0
Remi Torracinta <remit@uber.com>
2022-02-20 19:22:08 -0500

    Add neighborhood route

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/<neighborhood>")
+def is_neighborhood(neighborhood):
+    return f"You asked about {escape(neighborhood)}"
 
